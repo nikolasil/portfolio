f006d6aaeaffefd0c0a16a148b647bc24a921e3c
Nikolas <nikolasil2000@gmail.com>
2021-12-02 21:33:01 +0200

    done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,11 +4,9 @@ import './about.scss';
 export default function About() {
   return (
     <div className="about" id="about">
-      <div className="contact" id="contact">
-        <div className="container">
-          <div className="header">
-            <h1>About Me</h1>
-          </div>
+      <div className="container">
+        <div className="header">
+          <h1>About Me</h1>
         </div>
       </div>
     </div>
