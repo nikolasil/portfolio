3e327b36bb86fb60ad35fee00c3dc56ad80fdd55
Nikolas <nikolasil2000@gmail.com>
2021-12-06 17:24:27 +0200

    fixed warnings

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Menu, Close } from '@material-ui/icons';
 import './topbar.scss';
 import { isMobile } from 'react-device-detect';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
